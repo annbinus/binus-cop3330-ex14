27c97c5c6ecb495c0dba95a53d155f83a76fbca8
annbinus <72045490+annbinus@users.noreply.github.com>
2021-06-09 13:02:05 -0400

    Update TaxCal.java

--- src/main/java/base/TaxCal.java
+++ src/main/java/base/TaxCal.java
@@ -1,7 +1,7 @@
 package base;
 /*
  *  UCF COP3330 Summer 2021 Assignment 1 Solution
- *  Copyright 2021 first_name last_name
+ *  Copyright 2021 Ann Binus
 
 * Exercise 14 - Tax Calculator
 You don’t always need a complex control structure to solve simple problems. Sometimes a program requires an extra step in one case, but in all other cases there’s nothing to do.
